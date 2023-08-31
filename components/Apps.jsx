7a253dfb955d94cbc3e9cf37cdc24ac9d79252fd
Krunal <126875304+krunalbhongade@users.noreply.github.com>
2023-08-31 20:30:18 +0530

    Initial commit

---
+++ components/Apps.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Heading";
+import List from "./List";
+
+function Apps() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default Apps;
